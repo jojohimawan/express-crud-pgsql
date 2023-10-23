5548c2988c17b13ccafeb4cbde41927e145179b5
jojohimawan <jordan.hiphop21@gmail.com>
2023-10-23 22:43:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const env = process.env.NODE_ENV;
+
+const db = require('./queries');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({ 
+        extended: true 
+    })
+);
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`App running on port ${process.env.SERVER_PORT}.`)
+});
